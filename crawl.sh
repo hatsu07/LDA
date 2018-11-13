b4c4a025d832ddb954e3f398e60b30da30b08787
HAYATE <h@HAYATEnoMacBook-puro.local>
2018-11-13 17:32:43 +0900

    作業ファイルを全てUP

---
+++ crawl.sh
@@ -0,0 +1,20 @@
+echo 'Start'
+
+python crawl.py 食事
+
+python crawl.py 整容
+python crawl.py 洗顔
+python crawl.py 歯磨き
+python crawl.py 爪切り
+python crawl.py 耳かき
+
+python crawl.py 清拭
+python crawl.py 入浴
+
+python crawl.py 更衣
+python crawl.py 着替え
+
+python crawl.py トイレ動作
+python crawl.py 排泄
+
+echo 'Finish'
